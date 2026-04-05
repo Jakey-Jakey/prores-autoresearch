cb2edf9280c3f0284afdf16a88d4df9761c4fb9e
Jakey-Jakey <Jakey-Jakey@users.noreply.github.com>
2026-04-05 07:24:30 -0400

    Keep experiment: loop99 keep: raise standard br_tab to [1434,1118,966,886]

--- encoder_params.py
+++ encoder_params.py
@@ -76,7 +76,7 @@ PROFILES = {
         ],
         "min_quant": 1,
         "max_quant": 8,
-        "br_tab": [1428, 1113, 962, 882],
+        "br_tab": [1434, 1118, 966, 886],
     },
     "hq": {
         "luma_matrix": [
