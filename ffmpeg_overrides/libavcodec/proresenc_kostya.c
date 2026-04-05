673a119dbb0ac12514ebd724e1714a899cb134c3
Jakey-Jakey <Jakey-Jakey@users.noreply.github.com>
2026-04-05 10:23:01 -0400

    Keep experiment: trellis_soft_budget_headroom

--- ffmpeg_overrides/libavcodec/proresenc_kostya.c
+++ ffmpeg_overrides/libavcodec/proresenc_kostya.c
@@ -46,6 +46,8 @@ struct TrellisNode {
 };
 
 static const int prores_auto_rate_penalty_bits = 64;
+static const int prores_auto_budget_headroom_divisor = 4;
+static const int prores_auto_budget_penalty_bits = 16;
 
 typedef struct ProresThreadData {
     DECLARE_ALIGNED(16, int16_t, blocks)[MAX_PLANES][64 * 4 * MAX_MBS_PER_SLICE];
@@ -687,11 +689,19 @@ static int find_slice_quant(AVCodecContext *avctx,
         prev = trellis_node - TRELLIS_WIDTH + pq;
 
         for (q = min_quant; q < max_quant + 2; q++) {
+            int slice_budget;
+            int soft_limit;
             cur = trellis_node + q;
             bits  = td->nodes[prev].bits + slice_bits[q];
             error = slice_score[q];
             if (error < SCORE_LIMIT)
                 error += slice_bits[q] / prores_auto_rate_penalty_bits;
+            if (error < SCORE_LIMIT) {
+                slice_budget = ctx->bits_per_mb * mbs_per_slice;
+                soft_limit = bits_limit - slice_budget / prores_auto_budget_headroom_divisor;
+                if (bits > soft_limit)
+                    error += (bits - soft_limit) / prores_auto_budget_penalty_bits;
+            }
             if (bits > bits_limit)
                 error = SCORE_LIMIT;
 
